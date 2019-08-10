4944e18ab3c71f49dc44fb1205ebbce8225dbf00
Pratik Galoria <pgaloria@plex.com>
2019-08-10 14:54:25 +0530

    setup: Configure packages, webpack, eslint and server

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT ? process.env.PORT : 8181;
+const dist = path.join(__dirname, 'build');
+
+app.use(express.static(dist));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dist, 'index.html'));
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error); // eslint-disable-line no-console
+  }
+  console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
+});
---
+++ webpack.config.js
@@ -0,0 +1,143 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './build');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  context: sourcePath,
+  entry: {
+    app: './index.jsx',
+  },
+  output: {
+    path: outPath,
+    filename: isProduction ? '[contenthash].js' : '[hash].js',
+    chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].[hash].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      app: path.resolve(__dirname, 'src/app/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              sourceMap: !isProduction,
+              importLoaders: 1,
+              localIdentName: isProduction ? '[hash:base64:5]' : '[local]__[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer'),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              camelCase: true,
+              modules: true,
+              sourceMap: true,
+              importLoaders: 2,
+              localIdentName: '[name]_[local]_[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer'),
+              ],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.(a?png|svg)$/, use: 'url-loader?limit=10000' },
+      {
+        test: /\.(jpe?g|gif|bmp|mp3|mp4|ogg|wav|eot|ttf|woff|woff2)$/,
+        use: 'file-loader'
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: true,
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2,
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          filename: isProduction ? 'vendor.[contenthash].js' : 'vendor.[hash].js',
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: true,
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: false,
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[hash].css',
+      disable: !isProduction,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'assets/index.html',
+      base: { href: '/' },
+    }),
+  ],
+  devServer: {
+    contentBase: sourcePath,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    stats: 'minimal',
+    clientLogLevel: 'warning',
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8181',
+        changeOrigin: true,
+      },
+    },
+  },
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+};
